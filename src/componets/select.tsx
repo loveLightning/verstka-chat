7a9107a1d357f6b92757fa61e7852559d077b675
zaycevnet20 <74300821+zaycevnet20@users.noreply.github.com>
2022-11-14 19:06:47 +0300

    add select default

--- src/componets/select.tsx
+++ src/componets/select.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Select from 'react-select';
 import styled from 'styled-components';
 import { device } from '../utils/constants';
 import { sideBarItem } from './types';
+import arrow from '../assets/images/arrow.svg'
 
 interface Props {
     setTopicTitle: (topicTitle: string) => void
@@ -10,111 +10,142 @@ interface Props {
 }
 
 export const SelectComponent: React.FC<Props> = ({ setTopicTitle, topicTitle }) => {
-    const getValue = () => {
-        return topicTitle ? sideBarItem.find(c => c.value === topicTitle) : ''
-    }
 
-    const onChange = (newValue: any) => {
-        setTopicTitle(newValue.value)
+    const changeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setTopicTitle(e.target.value);
+
     }
 
     return (
         <Wrapper>
-            <StyledSelect
-                isSearchable={false}
-                value={getValue()}
-                options={sideBarItem}
-                onChange={onChange}
-                classNamePrefix={'Select'}
-            />
+            <Select value={topicTitle} onChange={(e) => changeSelect(e)}>
+                {sideBarItem?.map(el => (
+                    <Option>{el.title}</Option>
+                ))}
+            </Select>
         </Wrapper>
     );
 }
 
 const Wrapper = styled.div`
     display: none;
+    border-radius: 16px;
+    background: ${({theme}) => theme.darkGrey};
     @media ${device.tablet} {
         display: block;
         margin-bottom: 20px;
     }
 `
-const StyledSelect = styled(Select)`
-    .Select__control {
-        height: 55px;
-        width: 100%;
-        border-radius: 16px;
-        cursor: pointer;
-        background: ${({ theme }) => theme.darkGrey};
-        text-align: center;
+
+const Select = styled.select`
+    width: 100%;
+    height: 55px;
+    border: none;
+    outline: none;
+    color: ${({theme}) => theme.white};
+    text-align: center;
+    appearance: none;
+    border-radius: 16px;
+    background: ${({theme}) => theme.darkGrey};
+    position: relative;
+    :focus {
         outline: none;
-        box-shadow: none;
-        border: none;
+        border-radius: none;
     }
-    .Select__single-value {
-        color: ${({ theme }) => theme.white};
+    ::after {
+        display: block;
+        content: '';
+        height: 30px;
+        width: 30px;
+        background: url(${arrow}) no-repeat;
+        position: absolute;
+        z-index: 100;
     }
+`
+const Option = styled.option`
+    color: ${({theme}) => theme.white};
+    text-align: center;
+    background: ${({theme}) => theme.grey};
+`
 
-    .Select__value-container {
-        padding: 0;
-    }
-    .Select__control:hover {
-        outline: none;
-        border: none;
-    }
-    .Select__menu-list::-webkit-scrollbar {
-        width: 4px;
-        height: 0px;
-    }
-    .Select__menu-list::-webkit-scrollbar-track {
-        background: ${({ theme }) => theme.grey};
-    }
-    .Select__menu-list::-webkit-scrollbar-thumb {
-        background: ${({ theme }) => theme.white};
-    }
-    .Select__menu-list::-webkit-scrollbar-thumb:hover {
-        background: #555;
-    }
 
-    .Select__indicators {
-        color: ${({ theme }) => theme.grey};
-    }
+// const StyledSelect = styled(Select)`
+//     .Select__control {
+//         height: 55px;
+//         width: 100%;
+//         border-radius: 16px;
+//         cursor: pointer;
+//         background: ${({ theme }) => theme.darkGrey};
+//         text-align: center;
+//         outline: none;
+//         box-shadow: none;
+//         border: none;
+//     }
+//     .Select__single-value {
+//         color: ${({ theme }) => theme.white};
+//     }
 
-    .Select__dropdown-indicator {
-        color: ${({ theme }) => theme.grey};
-    }
+//     .Select__value-container {
+//         padding: 0;
+//     }
+//     .Select__control:hover {
+//         outline: none;
+//         border: none;
+//     }
+//     .Select__menu-list::-webkit-scrollbar {
+//         width: 4px;
+//         height: 0px;
+//     }
+//     .Select__menu-list::-webkit-scrollbar-track {
+//         background: ${({ theme }) => theme.grey};
+//     }
+//     .Select__menu-list::-webkit-scrollbar-thumb {
+//         background: ${({ theme }) => theme.white};
+//     }
+//     .Select__menu-list::-webkit-scrollbar-thumb:hover {
+//         background: #555;
+//     }
 
-    .Select__dropdown-indicator:hover {
-        color: ${({ theme }) => theme.grey};
-    }
+//     .Select__indicators {
+//         color: ${({ theme }) => theme.grey};
+//     }
 
-    .Select__menu {
-        margin: 0;
-    }
-    .Select__menu-list {
-        padding: 0;
-    }
+//     .Select__dropdown-indicator {
+//         color: ${({ theme }) => theme.grey};
+//     }
 
-    .Select__control--menu-is-open {
-        border-radius: 16px 16px 0 0;
-    }
+//     .Select__dropdown-indicator:hover {
+//         color: ${({ theme }) => theme.grey};
+//     }
 
-    .Select__control--is-focused {
-        box-shadow: 0 0 0 1px black;
-        outline: none;
-    }
+//     .Select__menu {
+//         margin: 0;
+//     }
+//     .Select__menu-list {
+//         padding: 0;
+//     }
 
-    .Select__indicator-separator {
-        display: none;
-    }
+//     .Select__control--menu-is-open {
+//         border-radius: 16px 16px 0 0;
+//     }
 
-    .Select__menu {
-        color: ${({ theme }) => theme.white};
-    }
+//     .Select__control--is-focused {
+//         box-shadow: 0 0 0 1px black;
+//         outline: none;
+//     }
 
-    .Select__option {
-        background: ${({ theme }) => theme.darkGrey};
-    }
-    .Select__option:hover {
-        background: ${({ theme }) => theme.grey};
-    }
-`;
+//     .Select__indicator-separator {
+//         display: none;
+//     }
+
+//     .Select__menu {
+//         color: ${({ theme }) => theme.white};
+//     }
+
+//     .Select__option {
+//         background: ${({ theme }) => theme.darkGrey};
+//     }
+//     .Select__option:hover {
+//         background: ${({ theme }) => theme.grey};
+//     }
+// `;
